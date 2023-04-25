eebfd7408f431bd70cc722dc660c2f0f2671ae07
leeviallu <68004427+leeviallu@users.noreply.github.com>
2023-04-25 20:02:11 +0300

    Frontti yhdistetty

--- index.js
+++ index.js
@@ -32,6 +32,7 @@ morgan.token('body', req => {
 
 app.use(cors())
 app.use(express.json())
+app.use(express.static('build'))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 app.get('/api/persons', (req, res) => {
