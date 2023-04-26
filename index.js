71e326b3e41ca525ededc1f34052592dfd4a8f59
leeviallu <68004427+leeviallu@users.noreply.github.com>
2023-04-26 21:53:01 +0300

    Toiminnot ovat yhteydessä DB, osio c valmis

--- index.js
+++ index.js
@@ -11,6 +11,21 @@ morgan.token('body', req => {
   return JSON.stringify(req.body)
 })
 
+const unknownEndpoint = (req, res) => {
+  res.status(404).send({error: 'unknown endpoint'})
+}
+
+const errorHandler = (error, req, res, next) => {
+  console.error('msg',error.message)
+  console.error('name',error.name)
+
+  if (error.name == 'CastError') {
+    res.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
+}
+
 app.use(cors())
 app.use(express.json())
 app.use(express.static('build'))
@@ -53,18 +68,44 @@ app.get('/info', (req, res) => {
     
 })
 
-app.get('/api/persons/:id', (req, res) => {
-  Person.findById(req.params.id).then(person => {
-    res.json(person)
-  })
+app.get('/api/persons/:id', (req, res, next) => {
+  Person.findById(req.params.id)
+    .then(person => {
+      if (person) {
+        res.json(person)
+      } else {
+        res.status(404).end()
+      }
+    })
+    .catch(error => next(error))
+})
+
+app.put('/api/persons/:id', (req, res, next) => {
+  const body = req.body
+
+  const person = {
+    name: body.name,
+    number: body.number,
+  }
+
+  Person.findByIdAndUpdate(req.params.id, person, { new: true })
+    .then(updatedPerson => {
+      res.json(updatedPerson)
+    })
+    .catch(error => next(error))
 })
 
 
-app.delete('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id)
-    persons = persons.filter(person => person.id !== id)
-    res.status(204).end()
+app.delete('/api/persons/:id', (req, res, next) => {
+    Person.findByIdAndRemove(req.params.id)
+      .then(response => {
+        res.status(204).end()
+      })
+      .catch(error => next(error))
 })
+
+app.use(unknownEndpoint)
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
